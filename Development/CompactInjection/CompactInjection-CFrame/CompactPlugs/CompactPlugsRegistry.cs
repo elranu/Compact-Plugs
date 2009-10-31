2d615bdba01d1930d88cf85976fcc0126c8e5142
elranu_cp <SND\elranu_cp>
2009-10-31 14:19:39 +0000

    Base compact plugs & builders in Compact Injection. Incomplete

---
+++ Development/CompactInjection/CompactInjection-CFrame/CompactPlugs/CompactPlugsRegistry.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using CompactPlugs_Primitives;
+
+namespace CompactPlugs
+{
+    class CompactPlugsRegistry
+    {
+
+        Dictionary<string, Plugin> LoadedPlugins;
+        Dictionary<string, Plugin> InstalledPlugins;
+        Dictionary<string, Plugin> InitialPlugins;
+        Dictionary<Type, object> Outputs;
+        Dictionary<Type, Plugin> LazyPlugins;
+        Dictionary<string, Plugin> EasyPlugins; // son aquellos plgins q tiene los callers declarados.
+    }
+}
