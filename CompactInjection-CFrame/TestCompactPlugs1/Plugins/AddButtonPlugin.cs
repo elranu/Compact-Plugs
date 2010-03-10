d50b2a6e7f22a01e23edb6ac10fe51474de2c2e0
Mariano Julio Vicario <elranu@users.noreply.github.com>
2010-03-10 12:41:51 +0000

    Some Fixes, and a lazy plugin in the test project

--- CompactInjection-CFrame/TestCompactPlugs1/Plugins/AddButtonPlugin.cs
+++ CompactInjection-CFrame/TestCompactPlugs1/Plugins/AddButtonPlugin.cs
@@ -2,6 +2,8 @@
 using System.Linq;
 using System.Collections.Generic;
 using System.Text;
+using CompactPlugs;
+using CompactInjection;
 
 namespace TestCompactPlugs1.Plugins
 {
@@ -22,10 +24,17 @@ namespace TestCompactPlugs1.Plugins
             // 
             button1.Location = new System.Drawing.Point(75, 38);
             button1.Name = "button1";
-            button1.Size = new System.Drawing.Size(72, 20);
+            button1.Size = new System.Drawing.Size(120, 20);
             button1.TabIndex = 0;
-            button1.Text = "Plugin Added!";
+            button1.Text = "Call Plugins!";
             WinForm.Controls.Add(button1);
+            button1.Click += new EventHandler(button1_Click);
+        }
+
+        void button1_Click(object sender, EventArgs e)
+        {
+            PluginEngine plugEngine = CompactConstructor.DefaultConstructor.New<PluginEngine>("PluginEngine");
+            plugEngine.CallPlugins(this);
         }
 
     }
---
+++ CompactInjection-CFrame/TestCompactPlugs1/Plugins/Plugin1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TestCompactPlugs1.Plugins
+{
+    public class Plugin1
+    {
+        public Form1 WinForm { get; set; }
+        
+        private System.Windows.Forms.Label label1;
+        const string lbltext1 = "Hello from Plugin 1!";
+        const string lbltext2 = "Bye from Plugin 1!";
+        Timer myTimer;
+        
+        public void RunPlugin1() 
+        {
+            label1 = new System.Windows.Forms.Label();
+            this.label1.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular);
+            this.label1.Location = new System.Drawing.Point(46, 136);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(151, 20);
+            this.label1.Text = lbltext1;
+            WinForm.Controls.Add(label1);
+            RunTimer();
+        }
+
+        private void RunTimer() 
+        { 
+            myTimer = new Timer();
+            myTimer.Tick += new EventHandler(myTimer_Tick);
+            myTimer.Interval = 1000;
+            myTimer.Enabled = true;
+        }
+
+        void myTimer_Tick(object sender, EventArgs e)
+        {
+            switch (label1.Text)
+            {
+                case lbltext1:
+                    label1.Text = lbltext2;
+                    break;
+                case lbltext2:
+                    label1.Text = lbltext1;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+    }
+}
