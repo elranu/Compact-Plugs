ba7889a7cfaaa73d9c37b22e2ee5cb7f21140574
Mariano Julio Vicario <elranu@users.noreply.github.com>
2010-03-24 20:39:26 +0000

    minor changes and add some todos

---
+++ CompactInjection-CFrame/CompactPlugs/PluginDefinitionBuilderByAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompactPlugs
+{
+    /// <summary>
+    /// this class is responsable to build a PluginDefinition from an object that has attributes
+    /// SO CP will support Plugins definitions by XML, objects and Attributes
+    /// TODO: next version
+    /// </summary>
+    internal class PluginDefinitionBuilderByAttribute
+    {
+    }
+}
